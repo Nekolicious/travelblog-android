cc37d954681716b1bb389efada75c2910ba9cfb8
unknown <sabilihaqrafli@gmail.com>
2022-01-03 10:36:12 +0700

    first commit

---
+++ app/src/main/java/com/travelblog/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.travelblog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private int loadtime=4000; //4 sec
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent home=new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },loadtime);
+    }
+}
